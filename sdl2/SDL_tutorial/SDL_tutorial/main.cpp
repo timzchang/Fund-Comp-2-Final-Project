b0d35dbfa5363b34a374a9c04d32d2c99b27f1b8
myonkorux <tchang2@nd.edu>
2015-03-13 22:00:17 -0700

    tchang's personal log

---
+++ sdl2/SDL_tutorial/SDL_tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_tutorial
+//
+//  Created by Tim Chang on 3/8/15.
+//  Copyright (c) 2015 Tim Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
