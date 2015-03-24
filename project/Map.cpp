da36cb24d6902e37526ecb2fd6bc5b0031431df7
Christopher Syers <csyers@nd.edu>
2015-03-24 14:35:31 -0400

    Debugged Map class

--- project/Map.cpp
+++ project/Map.cpp
@@ -8,29 +8,28 @@
 #include <SDL2/SDL_image.h>
 Map::Map(){
   map_image = NULL;
-  std::cout << "Hello\n";
 }
 
 Map::~Map(){
+  free();
+}
+
+void Map::free(){
   SDL_DestroyTexture(map_image);
   map_image = NULL;
-  std::cout << "Bye\n";
 }
 
 void Map::loadImage(std::string image_path, SDL_Renderer* renderer){
-  SDL_Texture* loadedTexture = NULL;
+  free();
   SDL_Surface* loadedSurface = IMG_Load(image_path.c_str());
-  // optimize?
-  loadedTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
-  if(loadedTexture == NULL){
+  map_image = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+  if(map_image == NULL){
     std::cout << "Texture not created: " << SDL_GetError() << std::endl;
   }else{
     image_width = loadedSurface->w;
     image_height = loadedSurface->h;
   }
   SDL_FreeSurface(loadedSurface);
-  map_image = loadedTexture;
-  SDL_DestroyTexture(loadedTexture);
   int w;
   int h;
   SDL_QueryTexture(map_image,NULL,NULL,&w,&h);
--- project/Map.h
+++ project/Map.h
@@ -15,6 +15,7 @@ class Map{
     void loadImage(std::string,SDL_Renderer*);	// loads SDL_Texture with path
     void loadVector(std::string);	// loads info about the map
     void render_map(SDL_Renderer*);
+    void free();
   private:
     SDL_Texture* map_image;		// image
     std::vector<std::vector <int> > tile_properties;		// vector for properties of tiles
