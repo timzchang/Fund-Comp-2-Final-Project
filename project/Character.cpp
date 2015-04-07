cd35abc185a2cdb1485077afab2d0ac69f5042f6
Lucas Unruh <lunruh1@nd.edu>
2015-04-07 14:00:49 -0400

    Character definition

--- project/Character.cpp
+++ project/Character.cpp
@@ -23,3 +23,51 @@ Character::~Character(){
   direction = 0;
   phase = 0;
 }
+
+void Character::draw(SDL_Renderer* renderer){
+  SDL_Rect destRect = {xpos*32, ypos*32, 32, 32};
+  SDL_Rect clipRect = {state*32, direction*32, 32, 32};
+  SDL_RenderCopy(renderer, character_texture, &clipRect, &destRect);
+}
+
+void Character::move(int move_direction){
+  switch(move_direction){
+    case 0:
+      ypos-=1;
+      break;
+    case 1:
+      xpos+=1;
+      break;
+    case 2:
+      ypos+=1;
+      break;
+    case 3:
+      xpos-=1;
+      break;
+  }
+}
+
+void Character::change_direction(int face_direction){
+  switch(face_direction){
+    case 0:
+      direction=3;
+      break;
+    case 1:
+      direction=2;
+      break;
+    case 2:
+      direction=0;
+      break;
+    case 3:
+      direction=1;
+      break;
+  }
+}
+
+void Character::set_phase(int new_phase){
+	phase=new_phase;
+}
+
+int Character::get_phase(){
+	return phase;
+}
