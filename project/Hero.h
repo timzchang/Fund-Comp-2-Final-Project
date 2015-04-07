9b45ffa67e257b2b26f547fb6cc912f22c0d62f8
Lucas Unruh <lunruh1@nd.edu>
2015-04-07 14:35:49 -0400

    Hero class definition

--- project/Hero.h
+++ project/Hero.h
@@ -2,11 +2,13 @@
 #define HERO_H
 #include<vector>
 #include<string>
+#include"Map.h"
 #include"Character.h"
 class Hero: public Character{
   public:
     Hero();
     Hero(string, int, int, SDL_Renderer*);
+    void check_valid_move(Map, int, int, int, vector<vector<int>>*);
   private:
     int attack;
     int defence;
