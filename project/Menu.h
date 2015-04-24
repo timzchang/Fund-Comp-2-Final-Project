e5a3e75fe358a5361640e007a6f9c3e19ac142a6
Lucas Unruh <lunruh1@nd.edu>
2015-04-24 02:34:04 -0400

    Draft of menu class

---
+++ project/Menu.h
@@ -0,0 +1,21 @@
+#ifndef MENU_H
+#define MENU_H
+
+class Menu(){
+  public:
+    Menu();
+    int display();
+    int instruction();
+};
+#endif
+
+Menu::Menu(){
+}
+
+int Menu::display(){
+
+}
+
+int Menu::instruction(){
+
+}
